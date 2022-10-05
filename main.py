2cadbe3433441dcec3ac6eb759855063c9ee90c6
Matthias Liew <92225474+MatthiasLiew@users.noreply.github.com>
2022-10-05 11:23:35 +0000

    idkk

--- main.py
+++ main.py
@@ -332,7 +332,7 @@ async def main() -> None:
         return Response()
     starlette_app = Starlette(
             routes=[
-                Route("/telegram", telegram, methods=["POST"])
+                Route("/", telegram, methods=["POST"])
             ]
         )
       
@@ -385,7 +385,7 @@ async def main() -> None:
     application.add_handler(CommandHandler("start", start))
     application.add_handler(new_question)
     application.add_handler(new_reply)
-    await application.bot.set_webhook(url = "https://lkc-med-telegram-bot.herokuapp.com/telegram")
+    await application.bot.set_webhook(url = "https://lkc-med-telegram-bot.herokuapp.com/")
 
     
   
