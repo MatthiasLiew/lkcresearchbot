9a2f63394e36481c27292706b45a5b0ce61ee73c
Matthias Liew <92225474+MatthiasLiew@users.noreply.github.com>
2022-10-14 06:40:28 +0000

    switched back to webhooks

--- main.py
+++ main.py
@@ -460,13 +460,12 @@ def main() -> None:
     ))
     application.add_handler(tele_question)
     application.add_handler(tele_reply)
-    #application.run_webhook(
-    #  listen = "0.0.0.0",
-    #  port = PORT,
-    #  url_path = TOKEN,
-    #  webhook_url = f"https://lkcresearchtest2-matthiasliew.koyeb.app/{TOKEN}"
-    #)
-    application.run_polling()
+    application.run_webhook(
+      listen = "0.0.0.0",
+      port = PORT,
+      url_path = TOKEN,
+      webhook_url = f"https://lkcresearchtest2-matthiasliew.koyeb.app/{TOKEN}"
+    )
 
 
     
